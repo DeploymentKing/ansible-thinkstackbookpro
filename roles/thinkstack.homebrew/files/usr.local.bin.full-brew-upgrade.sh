1536219042ea5c70a56e0b7fff6427dfc6761836
Lee Myring <mail@thinkstack.io>
2018-07-17 11:32:27 +0100

    Fixed brew upgrade script and updated installed brew items

--- roles/thinkstack.homebrew/files/usr.local.bin.full-brew-upgrade.sh
+++ roles/thinkstack.homebrew/files/usr.local.bin.full-brew-upgrade.sh
@@ -1,40 +1,20 @@
 #!/usr/bin/env bash
 
-(set -x; brew update;)
-(set -x; brew upgrade;)
-
-(set -x; brew cleanup;)
+set -euo pipefail
 
 red=`tput setaf 1`
 green=`tput setaf 2`
 yellow=`tput setaf 3`
 reset=`tput sgr0`
 
-casks=( $(brew cask list) )
+echo "${green}Performing brew update${reset}"
+(set -x; brew update;)
+(set -x; brew upgrade;)
+(set -x; brew cleanup;)
+(set -x; brew doctor;)
 
-# set up array of sensitive apps that should not be updated automatically (requires Bash 4)
-declare -A exclusions
-for constant in chefdk
-do
-    exclusions[$constant]=1
-done
-
-for cask in ${casks[@]}
-do
-    version=$(brew cask info $cask | sed -n "s/$cask:\ \(.*\)/\1/p")
-    installed=$(find "/usr/local/Caskroom/$cask" -type d -maxdepth 1 -maxdepth 1 -name "$version")
-
-    if [[ ${exclusions[$cask]} ]]; then
-        echo "${red}${cask}${reset} is ${yellow}excluded${reset} from updates."
-    else
-        if [[ -z $installed ]]; then
-            echo "${red}${cask}${reset} requires ${red}update${reset}."
-            (set -x; brew cask uninstall $cask --force;)
-            (set -x; brew cask install $cask --force;)
-        else
-            echo "${red}${cask}${reset} is ${green}up-to-date${reset}."
-        fi
-    fi
-done
-
+echo "${green}Performing brew cask update${reset}"
+(set -x; brew cask outdated;)
+(set -x; brew cask upgrade;)
 (set -x; brew cask cleanup;)
+(set -x; brew cask doctor;)
